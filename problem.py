2ee3716953f36cb40967a17e2524a9d5cba318dc
Hyunwoong Kang <hyunwoong.kang@leedotype.com>
2024-02-27 19:02:27 +0900

    python 개선

--- problem.py
+++ problem.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 import os
 
@@ -10,28 +11,29 @@ file = f"src/problems/p{num}.rs"
 if os.path.isfile(file):
     print(f"already exists: {num}")
     sys.exit()
-
-with open(file, 'w') as f:
-    f.write(f"""
-pub fn _solve(input: String) -> String {{
-    String::new()
-}}
-            
-#[cfg(test)]
-mod p{num}_tests {{
-    use super::*;
-
-    #[test]
-    fn test_case_1() {{
-        let input = "".to_string();
-        let output = "".to_string();
-        assert_eq!(_solve(input), output);
+else: 
+    with open(file, 'w') as f:
+        f.write(f"""
+    pub fn _solve(input: String) -> String {{
+        String::new()
     }}
-}}
-""")
+                
+    #[cfg(test)]
+    mod p{num}_tests {{
+        use super::*;
 
-with open("src/problems/mod.rs", "a") as f:
-    f.write(f"pub mod p{num};")
+        #[test]
+        fn test_case_1() {{
+            let input = "".to_string();
+            let output = "".to_string();
+            assert_eq!(_solve(input), output);
+        }}
+    }}
+    """)
+
+    with open("src/problems/mod.rs", "a") as f:
+        f.write(f"pub mod p{num};")
 
 
-print(sys.argv[1])
+subprocess.run(["cargo", "fmt"])
+subprocess.run(["code", file])
